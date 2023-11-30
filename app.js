759e74ec1a7c6c588d6412f155043a9bda138478
isha3024 <gandhiisha45516@gmail.com>
2023-11-30 17:09:10 +0530

    Add Other Sections

--- app.js
+++ app.js
@@ -183,4 +183,37 @@ let swiper = new Swiper('.book-table-img-slider', {
         el: '.swiper-pagination',
         clickable: true    
     }
-})
+});
+
+let teamSlider = new Swiper('.team-slider', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 2000,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true    
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1.2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 3,
+        }
+    }
+});
